1ed6c9a3f84eac91cc6039c18e338efefe0cffc6
D1204493 <D1204493@o365.fcu.edu.tw>
2024-05-11 05:14:23 +0800

    fix1

--- ig.js
+++ ig.js
@@ -47,7 +47,7 @@ document.getElementById('loginButton').onclick = function () {
 }
 
 document.getElementById("loginFB").addEventListener("click", function() {
-    window.location.href = "https://www.facebook.com/login.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26locale%3Dzh_TW%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26response_type%3Dcode%252Cgranted_scopes%26scope%3Demail%26state%3D%257B%2522fbLoginKey%2522%253A%2522q6nuakpddj83xelbwj1o760pu12iuuzuxrkbhi9migny1l4xe07%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Db789e603-31a4-4a3f-a8a3-e71969f67325%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%2522q6nuakpddj83xelbwj1o760pu12iuuzuxrkbhi9migny1l4xe07%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%23_%3D_&display=page&locale=zh_TW&pl_dbl=0";
+    window.location.href = "https://www.facebook.com/?locale=zh_TW";
 }); //log in with facebook的連結
 
 document.getElementById("forget").addEventListener("click", function() {
